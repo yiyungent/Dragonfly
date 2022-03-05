4ef364387f40b36aff531074b70e6a8da03242fd
yiyun <yiyungent@gmail.com>
2022-03-05 21:07:07 +0800

    feat(webapi,.github): 移除截图,add ci

--- src/WebApi/SettingsModel.cs
+++ src/WebApi/SettingsModel.cs
@@ -2,10 +2,10 @@
 {
     public class SettingsModel
     {
-        public long CacheMinutes { get; set; } = 60;
+        //public long CacheMinutes { get; set; } = 60;
 
-        public string ChromeDriverDirectory { get; set; } = "/app/tools/selenium/";
+        //public string ChromeDriverDirectory { get; set; } = "/app/tools/selenium/";
 
-        public string CacheModel { get; set; }
+        //public string CacheModel { get; set; }
     }
 }
