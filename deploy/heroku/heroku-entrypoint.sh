f8b879e45e8af13e488ce89cca00c89987ed30c8
yiyun <yiyungent@gmail.com>
2022-03-07 21:43:18 +0800

    ci: .github,deploy

--- deploy/heroku/heroku-entrypoint.sh
+++ deploy/heroku/heroku-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# 参考: https://github.com/Squidex/squidex/blob/master/packer/heroku/squidex/squidex.sh
-
-# 注意: $PORT 非常重要, Railway 必须监听此端口
+# region env
 export ASPNETCORE_URLS="http://+:$PORT"
 export ASPNETCORE_ENVIRONMENT="Production"
 export TZ="Asia/Shanghai"
+# endregion env
 
+# region PluginCore
 echo ${PLUGINCORE_ADMIN_USERNAME}
 echo ${PLUGINCORE_ADMIN_PASSWORD}
 
@@ -14,7 +14,8 @@ mkdir App_Data
 
 touch /app/App_Data/PluginCore.Config.json
 
-cat '/app/railway-PluginCore.Config.json' | sed "s/PLUGINCORE_ADMIN_USERNAME/${PLUGINCORE_ADMIN_USERNAME}/g" | tee '/app/App_Data/PluginCore.Config.json'
+cat '/app/heroku-PluginCore.Config.json' | sed "s/PLUGINCORE_ADMIN_USERNAME/${PLUGINCORE_ADMIN_USERNAME}/g" | tee '/app/App_Data/PluginCore.Config.json'
 cat '/app/App_Data/PluginCore.Config.json' | sed "s/PLUGINCORE_ADMIN_PASSWORD/${PLUGINCORE_ADMIN_PASSWORD}/g" | tee '/app/App_Data/PluginCore.Config.json'
+# endregion PluginCore
 
 dotnet WebApi.dll
